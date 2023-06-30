1b48ea05cfaa4f1c1c93d931d89a1b9c0ab0c432
Origami404 <Origami404@foxmail.com>
2023-06-30 22:47:35 +0800

    [single] finish RF

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,9 @@
 import chisel3.stage.ChiselStage
-import single_cycle.{InstDecoder, Toplevel}
+import single_cycle.{InstDecoder, RegFile, Toplevel}
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val verilogCode = ChiselStage.emitVerilog(new InstDecoder)
+    val verilogCode = ChiselStage.emitVerilog(new RegFile)
     print(verilogCode)
   }
 }
---
+++ src/main/scala/single_cycle/RegFile.scala
@@ -0,0 +1,26 @@
+package single_cycle
+
+import Chisel._
+import chisel3.util.ValidIO
+
+class RegFile extends Module {
+    val io = IO(new Bundle {
+        val read_addr_1 = Input(UInt(5.W))
+        val read_addr_2 = Input(UInt(5.W))
+
+        val write_addr = Input(ValidIO(UInt(5.W)))
+        val write_data = Input(UInt(32.W))
+
+        val read_data_1 = Output(UInt(32.W))
+        val read_data_2 = Output(UInt(32.W))
+    })
+
+    private val reg_file = Mem(31, UInt(32.W))
+
+    io.read_data_1 := Mux(io.read_addr_1 === 0.U, 0.U, reg_file(io.read_addr_1 - 1.U))
+    io.read_data_2 := Mux(io.read_addr_2 === 0.U, 0.U, reg_file(io.read_addr_2 - 1.U))
+
+    when (io.write_addr.valid && io.write_addr.bits =/= 0.U) {
+        reg_file(io.write_addr.bits - 1.U) := io.write_data
+    }
+}
